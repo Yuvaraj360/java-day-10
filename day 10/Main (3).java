56857fdc6aae5e094b21909c99a056ac0c694f37
Yuvaraj360 <137602148+Yuvaraj360@users.noreply.github.com>
2023-07-20 14:15:52 +0530

    Add files via upload

---
+++ day 10/Main (3).java
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+ import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+public int MostFrequentDigit(int input1, int input2, int input3, int input4){
+int arr[]={input1,input2,input3,input4};
+        int temp[]=new int[10];
+        int num;
+        for(int i=0;i<arr.length;i++)
+        {   num=arr[i];
+            while(num!=0)
+            {
+                int n=num%10;
+                temp[n]++;
+                num/=10;
+            }
+        }
+        int max=-1;
+        int x=0;
+        for(int i=0;i<temp.length;i++)
+        {
+            if(temp[i]>=max)
+            {
+                max=temp[i];
+                x=i;
+            }
+        }
+    return x;
+    }
+}
---
+++ day 10/Main.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+ hgvcsgwg*/
+ import java.io.*;
+import  java.util.*;
+class UserMainCode{
+
+public String secondWordUpperCase(String input1){
+
+String strArray[] = input1.split(" ");        
+
+String stt="LESS";        
+
+if(strArray.length==1) {            
+
+return stt;        
+
+}        
+
+return strArray[1].toUpperCase();   
+
+ }}
